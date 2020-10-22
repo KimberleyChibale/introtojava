76c9207029245addae69f630458100337cc18c2a
Kimberley Chibale <72111453+KimberleyChibale@users.noreply.github.com>
2020-10-23 01:43:52 +0200

    Add js changes

--- external.js
+++ external.js
@@ -1,5 +1,7 @@
 function greetKim(){
     alert("Hello Kim");
 }
-
+//single line comment
+/*this file was created by Kim,
+for explorer academy web development class */
 document.getElementById('myButton').onclick = greetKim;
